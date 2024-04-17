c60e7686cc6cd32f44f18154c082cfd9c822d1af
firmanputra908 <firmanputra908@gmail.com>
2024-04-17 19:28:41 +0700

    changing config.php base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://8.222.233.249/';
+$config['base_url'] = 'http://8.222.233.249/codeigniter';
 
 /*
 |--------------------------------------------------------------------------
